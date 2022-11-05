1e4981c9e3df6e0e19e2b546c6e50b2ed8d8e72e
Scott Enriquez <enriquezscott@mines.edu>
2022-11-05 17:57:56 -0400

    Initial commit

---
+++ code0.js
@@ -0,0 +1,21 @@
+gdjs.MainCode = {};
+
+gdjs.MainCode.conditionTrue_0 = {val:false};
+gdjs.MainCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.MainCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.MainCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.MainCode.eventsList0(runtimeScene);
+
+return;
+
+}
+
+gdjs['MainCode'] = gdjs.MainCode;
